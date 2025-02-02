550b24ce6033f34e908d5fde856596df416deafc
Alina.Shishonkova <90982871+Shishonkov20@users.noreply.github.com>
2025-02-02 19:48:54 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,12 +1,14 @@
-from django.urls import path
-from .views import *
-
+from main.views import *
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', MainPage.as_view(), name='main_page'),
-    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail_project'),
-    path('works_by_project/<int:pk>', works_by_project, name='works_by_project'),
-    path('register/', RegistrationFormView.as_view(), name='register'),
-    path('login/', MainLoginView.as_view(), name='login'),
-    path('logout/', MainLogoutView.as_view(), name='logout'),
+    path('admin/', admin.site.urls),
+    path('', include('main.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
